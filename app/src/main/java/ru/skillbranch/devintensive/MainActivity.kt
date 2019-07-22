b280f2bd0ef474672aeedf46eba75fedc04e435a
Quozar <quanter.box@gmail.com>
2019-07-22 15:44:31 +0300

    hometask_2 complete

--- app/src/main/java/ru/skillbranch/devintensive/MainActivity.kt
+++ app/src/main/java/ru/skillbranch/devintensive/MainActivity.kt
@@ -2,11 +2,39 @@ package ru.skillbranch.devintensive
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        Log.d("M_MainActivity","onCreate")
     }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d("M_MainActivity","onDestroy")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.d("M_MainActivity","onPause")
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Log.d("M_MainActivity","onStart")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.d("M_MainActivity","onStop")
+    }
+
+    override fun onRestart() {
+        super.onRestart()
+        Log.d("M_MainActivity","onRestart")
+    }
+
 }
