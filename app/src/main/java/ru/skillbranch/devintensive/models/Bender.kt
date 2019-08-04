f81a43e7f54f9d8da83a78d4db4e9f157aa08d1c
Quozar <quanter.box@gmail.com>
2019-08-04 18:14:59 +0300

    hometask_3 complete

--- app/src/main/java/ru/skillbranch/devintensive/models/Bender.kt
+++ app/src/main/java/ru/skillbranch/devintensive/models/Bender.kt
@@ -18,7 +18,11 @@ class Bender(var status: Status = Status.NORMAL, var question: Question = Questi
             "Отлично - ты справился\n${question.question}" to status.color
         }else{
             status = status.nextStatus()
-            "Это неправильный ответ. Давай все по новой\n${question.question}" to status.color
+            if (status == Status.NORMAL) {
+                question = Question.NAME
+                "Это неправильный ответ. Давай все по новой\n${question.question}" to status.color
+            }else
+                "Это неправильный ответ\n${question.question}" to status.color
         }
     }
 
